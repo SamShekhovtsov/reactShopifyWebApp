75cb69092ac09a63bf5f3ddd39af8fd8076f9972
Sem Shekhovtsov <semshehovtsov@gmail.com>
2020-06-14 15:01:17 +0300

    Polaris integration - adding styles

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import { Layout, Page, TextStyle } from '@shopify/polaris'
+
 const Index = () => (
-    <div>
-        <p>Sample app using React and Next.js</p>
-    </div>
+    <Page>
+        <Layout>
+            <p>Sample app using React and Next.js</p>
+            <TextStyle variation="positive">
+                Sample app using React and Next.js
+            </TextStyle>
+        </Layout>
+    </Page>
 )
 
 export default Index
